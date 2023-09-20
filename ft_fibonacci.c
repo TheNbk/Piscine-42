4e9c85123f61fcc97598add9a9dbfdd5e2832585
TheNbk <139504452+TheNbk@users.noreply.github.com>
2023-09-20 15:13:41 -0300

    Create ft_fibonacci.c

---
+++ ft_fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_fibonacci(int index)
+{
+    if (index < 0)
+        return (-1);
+    if (index == 0)
+        return (0);
+    if (index == 1)
+        return (1);
+    return (ft_fibonacci(n - 1) + ft_fibonacci(n - 2));
+}
