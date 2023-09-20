7797e8232e22a7cb58dccaa902d6a5b7b5b9f5f8
TheNbk <139504452+TheNbk@users.noreply.github.com>
2023-09-20 08:57:56 -0300

    Create ft_recursive_factorial.c

---
+++ ft_recursive_factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if(nb < 1)
+		return(0);
+	if(nb == 1)
+		return(1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+#include <stdio.h>
+int     main()
+{
+    printf ("%d", ft_recursive_factorial(3));
+}
